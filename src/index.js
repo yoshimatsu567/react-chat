e03cf61f5821c1a3fa7a9dd2c13018486d2c8478
yoshimatsu567 <lilkiossgang@gmail.com>
2021-09-13 00:03:49 +0900

    redux-persist追加(userNameデータ永続化)

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-import React from "react";
-import { Provider } from "react-redux";
-import ReactDOM from "react-dom";
-import ChatApp from "../src/templates/ChatApp";
-import { StylesProvider } from "@material-ui/core/styles";
-import { store } from "../src/reducks/store/store";
+import React from 'react';
+import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom';
+import ChatApp from '../src/templates/ChatApp';
+import { StylesProvider } from '@material-ui/core/styles';
+import { store } from '../src/reducks/store/store';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+
+let persistor = persistStore(store);
 
 ReactDOM.render(
   <StylesProvider injectFirst>
     <Provider store={store}>
-      <ChatApp />
+      <PersistGate loading={null} persistor={persistor}>
+        <ChatApp />
+      </PersistGate>
     </Provider>
   </StylesProvider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
--- src/reducks/store/store.js
+++ src/reducks/store/store.js
@@ -1,10 +1,20 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import { MessagesReducer, MessagesListReducer } from "../messages/reducers";
-import thunk from "redux-thunk";
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { MessagesReducer, MessagesListReducer } from '../messages/reducers';
+import thunk from 'redux-thunk';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 const allReducers = combineReducers({
   messages: MessagesReducer,
   messagesList: MessagesListReducer,
 });
 
-export const store = createStore(allReducers, applyMiddleware(thunk));
+const persistConfig = {
+  key: 'root',
+  whitelist: ['messages'],
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, allReducers);
+
+export const store = createStore(persistedReducer, applyMiddleware(thunk));
